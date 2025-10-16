e706e261410e0077bc2c88bfcfb7df107ea0486f
itu-itis21-gizaw21 <gizaw21@itu.edu.tr>
2025-10-16 11:49:34 +0300

    refactor code

--- src/test/schnorr.test.ts
+++ src/test/schnorr.test.ts
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { SchnorrSimulator } from "./bboard-simulator.js";
+import { SchnorrSimulator } from "./schnorr-simulator.js";
 import {
   NetworkId,
   setNetworkId,
